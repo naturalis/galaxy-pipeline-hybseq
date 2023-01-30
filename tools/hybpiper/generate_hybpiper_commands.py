f7aa9aee3b6a0bee131123c70f23782a7c3f2a8f
Jeremy van Veen <46000341+Eremus007@users.noreply.github.com>
2023-01-30 09:13:15 +0100

    Initial commit
    --Version 0.6.8
    
    shell script works in conda environment when comments are uncommented

---
+++ tools/hybpiper/generate_hybpiper_commands.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+import os, argparse
+
+
+def get_file_names(directory):
+    """Iterates through a specified directory and returns a list with
+    the file names.
+
+        Parameters
+        ----------
+        directory : str
+            The file containing the files to iterate through
+
+        Returns
+        -------
+        filenames : list
+            a list of strings which are the names of the files in the
+            specified directory.
+        """
+    # iterate over files in
+    # that directory
+    filenames = []
+    for filename in os.listdir(directory):
+        file = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(file):
+    # split off extentions
+            file_list = file.split('.')
+    # remove directory path/extentions and keep file name
+            filenames.append(file_list[0][1+len(directory):])
+    return filenames
+
+
+def get_sample_names(filenames, unique=False):
+    samplenames_list, pair_dict = [], {}
+    for filename in filenames:
+        part_index = 0
+        filename_parts = filename.split("_")
+        if "R1" in filename_parts:
+            part_index = filename_parts.index("R1")
+        elif "R2" in filename_parts:
+            part_index = filename_parts.index("R2")
+        samplename = filename_parts[:part_index][0]
+        samplenames_list.append(samplename)
+        if unique:
+            samplenames_list = set(samplenames_list)
+            samplenames_list = list(samplenames_list)
+            samplenames_list.sort()
+    return samplenames_list
+
+def write_commands_to_file(to_write_list, outputlocation="cmdfile.txt", overwrite=False):
+    file = outputlocation
+    if overwrite:
+        resetfile = open(file, "w")
+        resetfile.close()
+    with open(file, "a") as outfile:
+        for element in to_write_list:
+            outfile.write(element+"\n")
+
+
+def construct_assemble_commands(readfile, samplenames, target_format, targetfile, search_engine, intronerate_bool):
+    assemble_cmds = []
+    for sample in samplenames:
+        assemble_cmd = "hybpiper assemble -r %s/%s.fastq" % (str(readfile), sample)
+        if target_format == "AA" or target_format == "aa":
+            assemble_cmd= str(assemble_cmd) + " -t_aa %s" % targetfile
+        elif target_format == "DNA" or target_format == "dna":
+            assemble_cmd = str(assemble_cmd) + " -t_dna %s" % targetfile
+        assemble_cmd = str(assemble_cmd) + " --prefix %s" % sample
+
+        if search_engine == "diamond":
+            assemble_cmd = str(assemble_cmd) + " --diamond"
+        elif search_engine == "bwa":
+            assemble_cmd = str(assemble_cmd) + " --bwa"
+
+        if intronerate_bool == "y":
+            assemble_cmd = str(assemble_cmd) + " --run_intronerate"
+        # assemble_cmd = str(assemble_cmd) + " --hybpiper_dir %s" % str(output_location)
+
+        assemble_cmds.append(assemble_cmd)
+    return assemble_cmds
+
+
+def construct_stats_command():
+    stat_cmd = "hybpiper stats -t_dna test_targets.fasta gene namelist.txt"
+    return stat_cmd
+
+def construct_heatmap_command():
+    heatmap_cmd = "hybpiper recovery_heatmap seq_lengths.tsv"
+    return heatmap_cmd
+
+
+def construct_retrieve_commands():
+    retrieve_cmds = []
+    retrieve_cmd_1 = "hybpiper retrieve_sequences -t_dna test_targets.fasta dna --sample_names namelist.txt --fasta_dir 01_dna_seqs"
+    retrieve_cmd_2 = "hybpiper retrieve_sequences -t_dna test_targets.fasta aa --sample_names namelist.txt --fasta_dir 02_aa_seqs"
+    retrieve_cmds.append(retrieve_cmd_1)
+    retrieve_cmds.append(retrieve_cmd_2)
+
+    return retrieve_cmds
+
+def construct_paralog_command():
+    paralog_cmd = "hybpiper paralog_retriever namelist.txt -t_dna test_targets.fasta"
+    return paralog_cmd
+
+
+def parseArgvs():
+    parser = argparse.ArgumentParser(prog="generate_namelist.py",
+                                     description="Script to create a file "
+                                                 "containing the name of "
+                                                 "every sample in the readfile ")
+    parser.add_argument("-v", "--version", action="version",
+                        version="generate_namelist.py 1.0.1")
+    parser.add_argument("-r", "--readfile", action="store", dest="readfile",
+                        help="The location of the input readfile(s)",
+                        required=True)
+    parser.add_argument("-o", "--output", action="store", dest="output_path",
+                        help="The path location where the output file with the new commands should go",
+                        required=False)
+    parser.add_argument("-t", "--targets", action="store", dest="targetfile",
+                        help="The location of the input targetfile",
+                        required=True)
+    parser.add_argument("-f", "--target_format", action="store", dest="target_format",
+                        help="The type of sequences the targets are comprised off, either DNA or Amino acids.",
+                        required=True)
+    parser.add_argument("-e", "--engine", action="store", dest="search_engine",
+                        help="Which method Hybpiper uses to map the sequences to the targets.", required=True)
+    parser.add_argument("-i", "--intronerate", action="store", dest="intronerate_bool",
+                        help="Whether Hybpiper should extract the introns using intronerate", required=False)
+    parser.add_argument("-m", "--heatmap_bool", action="store", dest="heatmap_bool",
+                        help="Whether hybpiper should generate a heatmap of the gene recovery", required=False)
+    parser.add_argument("-g", "--output_g", action="store", dest="output_galaxy",
+                        help="The path location where the output should go for Galaxy",
+                        required=False)
+    argvs = parser.parse_args()
+    return argvs
+
+
+def main():
+    argvs = parseArgvs()
+    readfile = argvs.readfile
+    targetfile = argvs.targetfile
+    target_format = argvs.target_format
+    search_method = argvs.search_engine
+    intronerate_bool = argvs.intronerate_bool
+    galaxy_output_location = argvs.output_galaxy
+    heatmap_bool = argvs.heatmap_bool
+    output_location = argvs.output_path
+
+    filenameslist = get_file_names(readfile)
+    samplenameslist = get_sample_names(filenameslist, True)
+    # filenameslist = get_sample_names(filenames, True)
+    ## Write samplenames to namelist.txt
+    # if output_location == None:
+    #     write_Samplenames_to_file(samplenameslist)
+    # else:
+    #     write_Samplenames_to_file(samplenameslist, output_location)
+    cmds = construct_assemble_commands(readfile, filenameslist, target_format, targetfile, search_method, intronerate_bool)
+
+    stats_cmd = construct_stats_command()
+    heatmap_cmd = construct_heatmap_command()
+    retrieve_cmds = construct_retrieve_commands()
+    paralog_cmd = construct_paralog_command()
+
+
+    # write commands to .txt file
+    write_commands_to_file(cmds, overwrite=True)
+    write_commands_to_file([stats_cmd])
+    write_commands_to_file([heatmap_cmd])
+    write_commands_to_file(retrieve_cmds)
+    write_commands_to_file([paralog_cmd])
+
+if __name__ == '__main__':
+    main()
---
+++ tools/hybpiper/hybpiper_assemble.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# #!/usr/bin/env bash
+
+# sanity check
+#printf "Conda env: $CONDA_DEFAULT_ENV\n" >&1
+#printf "Biopython version: $(conda list | egrep biopython | awk '{print $2}')\n" >&1
+#printf "Samtools version: $(conda list | egrep samtools | awk '{print $2}')\n" >&1
+#printf "Unzip version: $(unzip -v | head -n1 | awk '{print $2}')\n" >&1
+#printf "Zip version: $(zip -v | head -n1 | awk '{print $2}')\n" >&1
+#printf "Bash version: ${BASH_VERSION}\n\n" >&1
+
+# The runDistributeToTargets function calls the python script, which,
+# after a BWA search against the target sequences, sorts the hits
+# from the BAM-file to the successful hits.
+# Then copies the output to one final ZIP file and removes the temporary files.
+
+# Usage: sh distribute_reads_to_targets_bwa.sh -b Galaxy82-[Merged_BAM].bam -r Raw_reads_test.zip -o distributed_reads.zip
+#
+
+runHybpiperAssemble() {
+    (
+    strScriptDir=$(dirname "$(readlink -f "$0")")
+#    strScriptDir="/home/eremus007/Desktop/Hybpiper/scripts"
+    base_location=$(echo ${outputzip} | egrep -o '^.*files')
+#    base_location="/home/eremus007/Desktop/Hybpiper/final_output/"
+    strDirectory=$(mktemp -d ${base_location}/XXXXXX)
+
+    mkdir -p "${strDirectory}_temp"
+    mkdir -p "${strDirectory}_temp/outputfile/"
+    outputDirectory=$(dirname "${readfiles}")
+
+
+    #Unzip readfile zip and copy to proper location
+    unzip ${readfiles} -d ${strDirectory}_temp/raw_reads
+    rawReadsFile="${strDirectory}_temp/raw_reads"
+
+    # Generate Hybpiper commands
+     python3 ${strScriptDir}/generate_hybpiper_commands.py \
+        -r ${rawReadsFile} \
+        -o "cmdfile.txt" \
+        -t ${targetfile} \
+        -f ${target_format} \
+        -e ${search_engine} \
+        -i ${intronerate_bool} \
+        -m ${heatmap_bool}
+
+    # Execute generated Hybpiper commands
+    while read cmd_to_execute
+      do
+        eval "${cmd_to_execute}"
+      done < cmdfile.txt
+
+    # move everything to proper location
+    cp -r ${outputDirectory}/* ${strDirectory}_temp/outputfile/
+
+    # Zip the correct output files in the temporary directory
+    zip -r ${strDirectory}_temp/tempzip.zip ${strDirectory}_temp/outputfile/*
+    # Copy the zip file to the galaxy data.dat
+    cp ${strDirectory}_temp/tempzip.zip ${outputzip}
+
+    # Delete remaining temporary files
+    rm -rf ${strDirectory}_temp
+
+    # Delete Generated commands txt file
+    rm ${strScriptDir}/cmdfile.txt
+
+    ) #> /dev/null 2>&1 #This will make sure nothing is written to stderr (hopefully)
+}
+
+# The main function.
+main() {
+  runHybpiperAssemble
+}
+
+# The getopts function.
+while getopts ":r:o:t:f:e:i:m:vh" opt; do
+    case ${opt} in
+        r)
+            readfiles=${OPTARG}
+            ;;
+        o)
+            outputzip=${OPTARG}
+            ;;
+        t)
+            targetfile=${OPTARG}
+            ;;
+        f)
+            target_format=${OPTARG}
+            ;;
+        e)
+            search_engine=${OPTARG}
+            ;;
+        i)
+            intronerate_bool=${OPTARG}
+            ;;
+        m)
+            heatmap_bool=${OPTARG}
+            ;;
+        v)
+            echo ""
+            echo "hybpiper_assemble.sh [x.x.x]"
+            echo ""
+
+            exit
+            ;;
+        h)
+            echo ""
+            echo "Usage: distribute_reads_to_targets_bwa.sh [-h] [-v] "
+            echo "                 [-r RAW_READS_ZIP]                 "
+            echo "                 [-o OUTPUT_ZIP]                    "
+            echo "                 [-t SEQUENCE_TYPE]                   "
+            echo ""
+            echo "HybPiper was designed for targeted sequence capture,"
+            echo "in which DNA sequencing libraries are enriched for gene regions of interest, especially for phylogenetics. "
+            echo "HybPiper is a suite of Python scripts/modules that wrap and connect bioinformatics tools "
+            echo "to extract target sequences from high-throughput DNA sequencing reads "
+            echo ""
+
+            exit
+            ;;
+        \?)
+            echo ""
+            echo "You've entered an invalid option: -${OPTARG}."
+            echo "Please use the -h option for correct formatting information."
+            echo ""
+
+            exit
+            ;;
+        :)
+            echo ""
+            echo "You've entered an invalid option: -${OPTARG}."
+            echo "Please use the -h option for correct formatting information."
+            echo ""
+
+            exit
+            ;;
+    esac
+done
+
+main
