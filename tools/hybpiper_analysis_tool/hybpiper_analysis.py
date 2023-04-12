e10c4174c5f3f839776278c0e35fcf6fcb48c0ec
Jeremy van Veen <jeremy.vanveen@hotmail.com>
2023-04-12 19:07:48 +0200

    Properly makes MSA's using muscle, added functionality to concatenate the input FNA files before alignment. Supermatrix and partition file generation yet to be implemented.

--- tools/hybpiper_analysis_tool/hybpiper_analysis.py
+++ tools/hybpiper_analysis_tool/hybpiper_analysis.py
@@ -20,6 +20,36 @@ from Bio.Align import MultipleSeqAlignment
 from Bio.SeqRecord import SeqRecord
 from Bio.Application import ApplicationError
 
+def concatenate_loci(input_dir, output_dir):
+    # Create a folder to store the concatenated files
+    concat_dir = os.path.join(output_dir, 'concatenated')
+    os.makedirs(concat_dir, exist_ok=True)
+
+    # Iterate over locus folders
+    for locus_folder in os.listdir(input_dir):
+        locus_folder_path = os.path.join(input_dir, locus_folder)
+        if os.path.isdir(locus_folder_path):
+            # create a list to store the SeqRecords for each entry in this locus
+            seq_records = []
+            # iterate over .FNA files in locus folder
+            for fna_file in os.listdir(locus_folder_path):
+                if fna_file.endswith('.FNA'):
+                    fna_file_path = os.path.join(locus_folder_path, fna_file)
+                    # get the sample name from the file name
+                    sample_name = fna_file.split('_')[0]
+                    # iterate over the SeqRecords in the FNA file
+                    for seq_record in SeqIO.parse(fna_file_path, 'fasta'):
+                        # set the SeqRecord's ID to the sample name
+                        seq_record.id = sample_name
+                        seq_record.description = ''
+                        seq_records.append(seq_record)
+            # write the SeqRecords for this locus to the output file
+            locus_output_file = os.path.join(concat_dir, locus_folder + '.fa')
+            with open(locus_output_file, 'w') as out_file:
+                SeqIO.write(seq_records, out_file, 'fasta')
+    return concat_dir
+
+
 
 def align_loci(input_dir, output_dir):
 
@@ -32,50 +62,47 @@ def align_loci(input_dir, output_dir):
 
     # Get the name of the current Conda environment
     env_name = os.environ.get("CONDA_DEFAULT_ENV")
+
     # Run the conda env list command to get a list of Conda environments and their paths
     output = subprocess.check_output([sys.executable, "-m", "conda", "env", "list"])
+
     # Convert the byte string output to a regular string and split it into lines
     output_lines = output.decode().split("\n")
+
     # Find the line that contains the current environment name
     env_line = next(line for line in output_lines if env_name in line)
+
     # Extract the path to the environment from the environment line
     env_path = env_line.split()[1]
+
     # Construct the path to the muscle executable
+    #muscle_path = os.path.abspath(os.path.join(env_path, "bin", "muscle"))
     muscle_path = shutil.which('muscle')
     if muscle_path is None:
         raise ValueError("Muscle executable not found in PATH")
-    # Create the output directory if it doesn't exist
-    os.makedirs(output_dir, exist_ok=True)
+
     # Create a subdirectory within the output directory to store the alignment files
     alignment_dir = os.path.join(output_dir, "alignments")
     os.makedirs(alignment_dir, exist_ok=True)
-    # For each locus folder, execute the other code
-    for locus_dir in os.listdir(input_dir):
-        locus_path = os.path.join(input_dir, locus_dir)
-        if not os.path.isdir(locus_path):
+
+    concat_dir = os.path.join(output_dir, "concatenated")
+    concat_path = os.listdir(concat_dir)
+    for concat_file in concat_path:
+        input_path = os.path.join(concat_dir, concat_file)
+        if "fna" in concat_file.lower().split("."):
+            output_file = concat_file.replace('.FNA', '.afa')
+        elif "faa" in concat_file.lower().split("."):
+            output_file = concat_file.replace('.FAA', '.afa')
+        elif "fa" in concat_file.lower().split("."):
+            output_file = concat_file.replace('.fa', '.afa')
+        output_path = os.path.join(alignment_dir, output_file)
+        muscle_cline = MuscleCommandline(str(muscle_path), input=input_path, out=output_path)
+        #print("Running Muscle with command: ", muscle_cline)
+        try:
+            stdout, stderr = muscle_cline()
+        except ApplicationError as e:
+            print("Muscle failed with error:", e.stderr)
             continue
-        # Check if file extention is among allowed file types.
-        fasta_files = [f for f in os.listdir(locus_path) if f.endswith('.FNA') or f.endswith('.FAA')]
-        if not fasta_files:
-            continue
-        # Create a subdirectory within the alignment directory for this locus
-        locus_alignment_dir = os.path.join(alignment_dir, locus_dir)
-        os.makedirs(locus_alignment_dir, exist_ok=True)
-        converted_files = []
-        for fasta_file in fasta_files:
-            input_path = os.path.join(locus_path, fasta_file)
-            if "fna" in fasta_file.lower().split("."):
-                output_file = fasta_file.replace('.FNA', '.afa')
-            elif "fa" in fasta_file.lower().split("."):
-                output_file = fasta_file.replace('.fa', '.afa')
-            output_path = os.path.join(locus_alignment_dir, output_file)
-            muscle_cline = MuscleCommandline(str(muscle_path), input=input_path, out=output_path)
-            print("Running Muscle with command: ", muscle_cline)
-            try:
-                stdout, stderr = muscle_cline()
-            except ApplicationError as e:
-                print("Muscle failed with error:", e.stderr)
-                continue
     return alignment_dir
 
 
@@ -84,26 +111,41 @@ def make_supermatrix(alignment_dir, output_dir):
     supermatrix and saves the resulting alignment in output_dir.
     """
     alignments = []
+
     for locus_dir in os.listdir(alignment_dir):
         locus_path = os.path.join(alignment_dir, locus_dir)
+
         if not os.path.isdir(locus_path):
             continue
+
         alignment_files = [f for f in os.listdir(locus_path) if f.endswith('.afa')]
+
         if not alignment_files:
             continue
+
         for alignment_file in alignment_files:
             input_path = os.path.join(locus_path, alignment_file)
+
+            # Add a check for file extension before parsing the file as a fasta file
+            if not input_path.endswith('.afa'):
+                continue
+
             with open(input_path) as f:
                 alignment = AlignIO.read(f, 'fasta')
                 alignments.append(alignment)
+
     supermatrix = MultipleSeqAlignment(alignments)
+
     for record in supermatrix:
         if 'X' in record.seq:
             record.seq = SeqRecord(seq=record.seq.ungap('X'), id=record.id, name=record.name, description=record.description).seq
+
     supermatrix_file = os.path.join(output_dir, 'supermatrix.phy')
     with open(supermatrix_file, 'w') as f:
         SeqIO.write(supermatrix, f, 'phylip')
     return supermatrix_file
+
+
 
 
 def make_partition_file(supermatrix_file, output_dir):
@@ -140,8 +182,9 @@ def main():
     input_dir = argvs.input_dir
     output_dir = argvs.output_dir
 
-    alignment_dir = align_loci(input_dir, output_dir)
-    #supermatrix_file = make_supermatrix(alignment_dir, output_dir)
+    concatenated_input_dir = concatenate_loci(input_dir, output_dir)
+    alignment_dir = align_loci(concatenated_input_dir, output_dir)
+    supermatrix_file = make_supermatrix(alignment_dir, output_dir)
     #make_partition_file(supermatrix_file, output_dir)
 
     #print("Input dir: " + str(input_dir))
