36c4c944f874b1576e9db544df368cecbc0eff4e
Jeremy van Veen <jeremy.vanveen@hotmail.com>
2023-04-14 15:13:21 +0200

    Python script completely functional, incorrect supermatrix, needs fixing still
    Python script completely functional, incorrect supermatrix, needs fixing still.
    
    (See make_supermatrix() function)

--- tools/hybpiper_analysis_tool/hybpiper_analysis.py
+++ tools/hybpiper_analysis_tool/hybpiper_analysis.py
@@ -14,11 +14,9 @@ import argparse
 import subprocess
 import sys
 from Bio.Align.Applications import MuscleCommandline
-from Bio import AlignIO
-from Bio import SeqIO
-from Bio.Align import MultipleSeqAlignment
-from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO, AlignIO
 from Bio.Application import ApplicationError
+
 
 def concatenate_loci(input_dir, output_dir):
     # Create a folder to store the concatenated files
@@ -50,33 +48,12 @@ def concatenate_loci(input_dir, output_dir):
     return concat_dir
 
 
-
-def align_loci(input_dir, output_dir):
-
+def align_loci(input_dir, output_dir, output_format='.phy'):
     """Performs sequence alignment for each locus in input_dir and
-    saves the resulting alignments in output_dir.
-
+    saves the resulting alignments in output_dir in the specified format.
+    The sequences will be aligned to the same length, using gaps if necessary.
     """
-
-    ### Get path to muscle.exe ###
-
-    # Get the name of the current Conda environment
-    env_name = os.environ.get("CONDA_DEFAULT_ENV")
-
-    # Run the conda env list command to get a list of Conda environments and their paths
-    output = subprocess.check_output([sys.executable, "-m", "conda", "env", "list"])
-
-    # Convert the byte string output to a regular string and split it into lines
-    output_lines = output.decode().split("\n")
-
-    # Find the line that contains the current environment name
-    env_line = next(line for line in output_lines if env_name in line)
-
-    # Extract the path to the environment from the environment line
-    env_path = env_line.split()[1]
-
-    # Construct the path to the muscle executable
-    #muscle_path = os.path.abspath(os.path.join(env_path, "bin", "muscle"))
+    # Get the path to the muscle executable
     muscle_path = shutil.which('muscle')
     if muscle_path is None:
         raise ValueError("Muscle executable not found in PATH")
@@ -87,80 +64,93 @@ def align_loci(input_dir, output_dir):
 
     concat_dir = os.path.join(output_dir, "concatenated")
     concat_path = os.listdir(concat_dir)
+
     for concat_file in concat_path:
         input_path = os.path.join(concat_dir, concat_file)
         if "fna" in concat_file.lower().split("."):
-            output_file = concat_file.replace('.FNA', '.afa')
+            output_file = concat_file.replace('.fna', output_format)
         elif "faa" in concat_file.lower().split("."):
-            output_file = concat_file.replace('.FAA', '.afa')
+            output_file = concat_file.replace('.faa', output_format)
         elif "fa" in concat_file.lower().split("."):
-            output_file = concat_file.replace('.fa', '.afa')
+            output_file = concat_file.replace('.fa', output_format)
+        else:
+            continue
+
         output_path = os.path.join(alignment_dir, output_file)
+        # Align sequences using Muscle
         muscle_cline = MuscleCommandline(str(muscle_path), input=input_path, out=output_path)
-        #print("Running Muscle with command: ", muscle_cline)
         try:
             stdout, stderr = muscle_cline()
         except ApplicationError as e:
             print("Muscle failed with error:", e.stderr)
             continue
+
+        # Open the alignment file and align sequences to the same length
+        with open(output_path, "r") as alignment_file:
+            alignment = AlignIO.read(alignment_file, "fasta")
+            alignment_len = alignment.get_alignment_length()
+            for record in alignment:
+                if len(record.seq) < alignment_len:
+                    # If sequence is shorter than the alignment length, add gaps to the end
+                    gap_len = alignment_len - len(record.seq)
+                    gap_seq = "-" * gap_len
+                    record.seq += gap_seq
+                elif len(record.seq) > alignment_len:
+                    # If sequence is longer than the alignment length, truncate it
+                    record.seq = record.seq[:alignment_len]
+
+        # Write the aligned sequences to the same output file in the specified format
+        with open(output_path, "w") as output_file:
+            AlignIO.write(alignment, output_file, "phylip")
+
     return alignment_dir
 
 
-def make_supermatrix(alignment_dir, output_dir):
-    """Concatenates alignment files from alignment_dir into a
-    supermatrix and saves the resulting alignment in output_dir.
-    """
-    alignments = []
+import os
 
-    for locus_dir in os.listdir(alignment_dir):
-        locus_path = os.path.join(alignment_dir, locus_dir)
+def create_supermatrix(input_folder, output_folder):
+    # Create a list of all the input file paths
+    input_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.endswith('.phy')]
 
-        if not os.path.isdir(locus_path):
-            continue
+    # Determine the number of loci and the length of the alignment for each file
+    num_loci = []
+    alignment_lengths = []
+    for file in input_files:
+        with open(file, 'r') as f:
+            num_sequences, alignment_length = f.readline().split()
+            num_sequences = int(num_sequences)
+            alignment_length = int(alignment_length)
+            num_missing = 0
+            for line in f:
+                if line.startswith(' '):
+                    num_missing += 1
+            num_samples = num_sequences - num_missing
+            num_loci.append(num_samples)
+            alignment_lengths.append(alignment_length)
 
-        alignment_files = [f for f in os.listdir(locus_path) if f.endswith('.afa')]
+    # Create the partition definition block
+    num_partitions = len(num_loci)
+    partition_sizes = ['{}-{}'.format(sum(num_loci[:i])+1, sum(num_loci[:i+1])) for i in range(num_partitions)]
+    partition_types = ['DNA' for i in range(num_partitions)]
+    partition_def = '{} {} {}\n'.format(num_partitions, ' '.join(partition_types), ' '.join(partition_sizes))
 
-        if not alignment_files:
-            continue
+    # Create the partition.txt file
+    with open(os.path.join(output_folder, 'partition.txt'), 'w') as f:
+        for i in range(num_partitions):
+            f.write('{} locus{}\n'.format(partition_sizes[i], i+1))
 
-        for alignment_file in alignment_files:
-            input_path = os.path.join(locus_path, alignment_file)
+    # Create the output supermatrix file
+    with open(os.path.join(output_folder, 'supermatrix.phy'), 'w') as out_file:
+        out_file.write(partition_def)
+        for i in range(num_partitions):
+            with open(input_files[i], 'r') as in_file:
+                in_file.readline()
+                for line in in_file:
+                    if line.startswith(' '):
+                        out_file.write('?' * alignment_lengths[i] + '\n')
+                    else:
+                        out_file.write(line)
 
-            # Add a check for file extension before parsing the file as a fasta file
-            if not input_path.endswith('.afa'):
-                continue
-
-            with open(input_path) as f:
-                alignment = AlignIO.read(f, 'fasta')
-                alignments.append(alignment)
-
-    supermatrix = MultipleSeqAlignment(alignments)
-
-    for record in supermatrix:
-        if 'X' in record.seq:
-            record.seq = SeqRecord(seq=record.seq.ungap('X'), id=record.id, name=record.name, description=record.description).seq
-
-    supermatrix_file = os.path.join(output_dir, 'supermatrix.phy')
-    with open(supermatrix_file, 'w') as f:
-        SeqIO.write(supermatrix, f, 'phylip')
-    return supermatrix_file
-
-
-
-
-def make_partition_file(supermatrix_file, output_dir):
-    """Generates a partition file for supermatrix_file and saves it
-    in output_dir.
-    """
-    with open(supermatrix_file) as f:
-        alignment = AlignIO.read(f, 'phylip')
-    partition_file = os.path.join(output_dir, 'partitions.txt')
-    with open(partition_file, 'w') as f:
-        for i, column in enumerate(alignment):
-            start = i+1
-            end = i+2
-            gene_name = f'gene{i+1}'
-            f.write(f'{start}-{end} {gene_name}\n')
 
 
 def parse_argvs():
@@ -178,19 +168,21 @@ def parse_argvs():
 
 
 def main():
+    LINE_CLEAR = '\x1b[2K'
     argvs = parse_argvs()
     input_dir = argvs.input_dir
     output_dir = argvs.output_dir
 
+    print("Concatenating input files...", end="\r")
     concatenated_input_dir = concatenate_loci(input_dir, output_dir)
+    print(end=LINE_CLEAR)
+    print("Running MUSCLE...", end="\r")
     alignment_dir = align_loci(concatenated_input_dir, output_dir)
-    supermatrix_file = make_supermatrix(alignment_dir, output_dir)
-    #make_partition_file(supermatrix_file, output_dir)
-
-    #print("Input dir: " + str(input_dir))
-    #print("Output dir: " + str(output_dir))
-    #print("Alignment_dir: " + str(alignment_dir))
-    # print("Supermatrix_file: " + str(supermatrix_file))
+    print(end=LINE_CLEAR)
+    print("Generating supermatrix...", end="\r")
+    create_supermatrix(alignment_dir, output_dir)
+    print(end=LINE_CLEAR)
+    print("Done!", end="\r")
 
 
 if __name__ == '__main__':
